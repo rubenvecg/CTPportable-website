5050ce51f80ab205a07e2947d4fe134997a7e063
Anastasiya <anastasiyauraleva@gmail.com>
2020-08-19 17:24:44 -0400

    added cards

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import './App.css';
 import City from './City/City.js';
 import Menu from './Menu/Menu.js'
+import Cards from './Cards/Cards.js'
 
 
 function App() {
   return (
     <div className="App">
-      <City/>
-      <Menu/>
+     <City/>
+     <Menu/>
+
+      <Cards/>
       <div className='container page-section'>asd</div>
     </div>
   );
